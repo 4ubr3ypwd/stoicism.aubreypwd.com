077744ac701c1e878c3cab37ce6c2b84923cc4f2
Aubrey Portwood <aubreypwd@gmail.com>
2017-05-10 09:38:39 -0700

    Add comments to make more clear.

--- wp-content/plugins/stoicism-aubreypwd-com/class-cmb2-loader.php
+++ wp-content/plugins/stoicism-aubreypwd-com/class-cmb2-loader.php
@@ -13,6 +13,8 @@ class CMB2_Loader {
 	/**
 	 * Was CMB2 loaded or what?
 	 *
+	 * Default to false unless changed by loader.
+	 *
 	 * @author Aubrey Portwood
 	 * @since  1.0.0
 	 *
@@ -27,10 +29,14 @@ class CMB2_Loader {
 	 * @since  1.0.0
 	 */
 	function __construct() {
+
+		// The file to load.
 		$file = WP_PLUGIN_DIR . '/cmb2/init.php';
 
 		// Load the CMB2 stuff.
 		if ( stream_resolve_include_path( $file ) && require_once( $file ) ) {
+
+			// We loaded the file!
 			$this->loaded = true;
 		}
 	}
