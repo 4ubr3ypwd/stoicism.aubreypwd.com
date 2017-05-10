673ddd923bd897aa7f051beac6e6ce0f0d171a44
Aubrey Portwood <aubreypwd@gmail.com>
2017-05-10 10:32:49 -0700

    Display the libaray link.

--- wp-content/plugins/stoicism-aubreypwd-com/templates/content-library.php
+++ wp-content/plugins/stoicism-aubreypwd-com/templates/content-library.php
@@ -1,1 +1,12 @@
-hi
+<?php
+/**
+ * Library single content.
+ *
+ * @since  1.0.0
+ */
+?>
+
+<dl>
+	<dt><?php esc_html_e( 'Link', 'aubreypwd-stoicism' ); ?></dt>
+	<dd><?php display_the_library_link(); ?></dd>
+</dl>
