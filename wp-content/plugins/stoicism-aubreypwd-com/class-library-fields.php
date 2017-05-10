20f2c0c99769a21aedcdd6d6204fcf2f70d47b39
Aubrey Portwood <aubreypwd@gmail.com>
2017-05-10 09:38:28 -0700

    Add normal and affilate links for library.

--- wp-content/plugins/stoicism-aubreypwd-com/class-library-fields.php
+++ wp-content/plugins/stoicism-aubreypwd-com/class-library-fields.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace aubreypwd\Stoicism;
+use \CMB2;
 
 /**
  * Library fields.
@@ -9,6 +10,16 @@ namespace aubreypwd\Stoicism;
  * @since  1.0.0
  */
 class Library_Fields {
+
+	/**
+	 * CMB2 metabox.
+	 *
+	 * @author Aubrey Portwood
+	 * @since  1.0.0
+	 *
+	 * @var CMB2
+	 */
+	public $metabox;
 
 	/**
 	 * Create CMB2 Fields.
@@ -24,5 +35,50 @@ class Library_Fields {
 			// The CMB2 plugin must not be activated.
 			return;
 		}
+
+		// Add fields.
+		add_action( 'cmb2_admin_init', array( $this, 'metabox' ) );
+	}
+
+	/**
+	 * Create the metabox.
+	 *
+	 * @author Aubrey Portwood
+	 * @since  1.0.0
+	 */
+	public function metabox() {
+		$this->metabox = new CMB2( array(
+			'id'           => 'aubreypwd-stoicism-library-fields',
+			'title'        => esc_html__( 'Library', 'aubreypwd-stoicism' ),
+			'object_types' => array( 'library' ),
+		) );
+
+		// Add fields to this.
+		$this->fields();
+	}
+
+	/**
+	 * Add the fields.
+	 *
+	 * @author Aubrey Portwood
+	 * @since  1.0.0
+	 */
+	public function fields() {
+
+		// Affiliate link.
+		$this->metabox->add_field( array(
+			'name'     => esc_html__( 'Affiliate URL', 'aubreypwd-stoicism' ),
+			'desc'     => esc_html__( 'Affiliate URL. Will use this first.', 'aubreypwd-stoicism' ),
+			'id'       => 'affiliate_url',
+			'type'     => 'text_url',
+		) );
+
+		// Normal link.
+		$this->metabox->add_field( array(
+			'name'     => esc_html__( 'Normal URL', 'aubreypwd-stoicism' ),
+			'desc'     => esc_html__( 'The non-affiliate normal URL to the resource. Used when there is no affiliate link.', 'aubreypwd-stoicism' ),
+			'id'       => 'normal_url',
+			'type'     => 'text_url',
+		) );
 	}
 }
