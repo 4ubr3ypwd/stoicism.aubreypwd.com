7c5c15a4d1eca860d5be1af15a4f82f3d0ec649f
Aubrey Portwood <aubreypwd@gmail.com>
2017-05-10 15:11:03 -0700

    Configure steps template.

--- wp-content/plugins/stoicism-aubreypwd-com/templates/baby-step-level-taxonomy.php
+++ wp-content/plugins/stoicism-aubreypwd-com/templates/baby-step-level-taxonomy.php
@@ -7,17 +7,27 @@
 
 get_header(); ?>
 
-<h1><?php single_cat_title(); ?></h1>
+<header class="page-header">
+	<h1 class="page-title"><?php single_cat_title(); ?></h1>
+
+	<!-- The term description -->
+	<p class="term-description entry-content"><?php echo term_description( get_queried_object_id(), 'baby-step-level' ); ?></p>
+</header>
 
 <?php if ( have_posts() ) : ?>
 
-	<ol>
+	<!-- The list of posts in that term -->
 		<?php while ( have_posts() ) : the_post(); ?>
-			<li>
-				<a href="<?php the_permalink(); ?>"><?php the_title() ?></a>
-			</li>
+			<article>
+				<header class="page-header">
+					<h4><a href="<?php get_the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
+				</header>
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+			</article>
 		<?php endwhile; ?>
-	</ol>
 
 <?php else : ?>
 
